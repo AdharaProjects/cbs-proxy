59d0b83803c30e346495d89219f74319eee31f85
coeniebeyers <coeniebeyers@gmail.com>
2018-06-08 12:54:38 +0200

    #2: Added transfers and transfers/{key} client calls.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ async function getAuth(username, password){
     }
   }
   try{
-    let response = await rp(options)
+    const response = await rp(options)
     return response
   } catch(err){
     console.log('ERROR with calling /api/auth:', err)
@@ -26,7 +26,55 @@ async function getAuth(username, password){
   }
 }
 
+async function getTransfers(username, password){
+  const credentials = username+':'+password
+  const base64Credentials = Buffer.from(credentials).toString('base64')
+  const options = {
+    method: 'GET',
+    uri: config.apiServerAddress + '/api/transfers',
+    json: true,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic '+base64Credentials
+    }
+  }
+  try{
+    const response = await rp(options)
+    return response
+  } catch(err){
+    console.log('ERROR with calling /api/transfers:', err)
+    return {
+      result: false
+    }
+  }
+}
+
+async function getTransferByKey(username, password, key){
+  const credentials = username+':'+password
+  const base64Credentials = Buffer.from(credentials).toString('base64')
+  const options = {
+    method: 'GET',
+    uri: config.apiServerAddress + '/api/transfers/'+key,
+    json: true,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic '+base64Credentials
+    }
+  }
+  try{
+    const response = await rp(options)
+    return response
+  } catch(err){
+    console.log('ERROR with calling /api/transfers/{key}:', err)
+    return {
+      result: false
+    }
+  }
+}
+
 module.exports = {
   getAuth,
+  getTransfers,
+  getTransferByKey,
   config
 }
--- test/index.js
+++ test/index.js
@@ -1,15 +1,27 @@
 const expect = require('chai').expect
 const index = require('../index.js')
 
-const username = 'user1'
-const password = '12345678'
+const username = 'coenie'
+const password = 'n[RI>LVc^OWs'
 
 describe("index.js", function() {
   this.timeout(10*1000)
 
   it("Route: api/auth - Returns data about the currently authenticated user", async function() {
-    let response = await index.getAuth(username, password)
+    const response = await index.getAuth(username, password)
     expect(response.user).to.not.be.undefined
+  })
+
+  it("Route: api/transfers - Searches for transfers over multiple accounts", async function() {
+    const response = await index.getTransfers(username, password)
+    expect(response[0].id).to.be.a('string')
+  })
+
+  it("Route: api/transfers/{key} - Returns details about a transfer", async function() {
+    const transfers = await index.getTransfers(username, password)
+    const key = transfers[0].id
+    const transferDetail = await index.getTransferByKey(username, password, key)
+    expect(transferDetail.transaction).to.not.be.undefined
   })
 })
 
